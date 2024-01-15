4b65d986ae6572af0b79476a3c0b1a1125562509
Fabián Huertas <miloh9318@hotmail.com>
2024-01-15 12:07:16 -0500

    agregando archivos

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // La parte mas importante es la seccion de content
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
